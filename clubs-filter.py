277df80077e900c02d5e2e43e7deba4118a9bc39
Stijn Grievink <83001732+stijngrie@users.noreply.github.com>
2024-10-19 16:01:01 +0200

    New club system

--- clubs-filter.py
+++ clubs-filter.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import top_clubs
 
 # Open the CSV file
 with open('Raw-Data-20242025\clubs.csv', mode='r', encoding='utf-8') as file:
@@ -10,16 +11,18 @@ with open('Raw-Data-20242025\clubs.csv', mode='r', encoding='utf-8') as file:
     # Filter out rows where 'last_season' is not equal to 2024
     new_rows = [row for row in clubsfile if row['last_season'] == '2024']
 
-    filtered_rows = [];
+    filtered_rows = []
     for row in new_rows:
         # Use dictionary comprehension to remove the unwanted columns
-        if (row['domestic_competition_id'] in ['GB1' , 'IT1', 'ES1', 'FR1', 'L1']):
-            filtered_row = {key: value for key, value in row.items() if key not in ['stadium_seats', 'national_team_players', 'last_season', 'domestic_competition_id', 'stadium_name', 'foreigners_number', 'average_age', 'total_market_value', 'foreigners_percentage', 'coach_name', 'net_transfer_record', 'squad_size', 'filename']}
+        if (int(row['club_id']) in top_clubs.clubs):
+            filtered_row = {key: value for key, value in row.items() if key not in ['stadium_seats', 'national_team_players', 'last_season', 'stadium_name',
+                                                                                    'foreigners_number', 'average_age', 'total_market_value', 'foreigners_percentage', 'coach_name', 'net_transfer_record', 'squad_size', 'filename', 'domestic_competition_id']}
             filtered_rows.append(filtered_row)
 
 # Open a new JSON file for writing
 with open('Filtered-Data-20242025/clubs.json', mode='w', encoding='utf-8') as jsonfile:
     # Write the filtered rows to the JSON file
-    json.dump(filtered_rows, jsonfile, indent=4, ensure_ascii=False)  # 'indent=4' makes the JSON output readable
+    # 'indent=4' makes the JSON output readable
+    json.dump(filtered_rows, jsonfile, indent=4, ensure_ascii=False)
 
 print("Filtered data has been saved to 'clubs.json'.")
---
+++ top_clubs.py
@@ -0,0 +1,32 @@
+clubs = [
+    281,  # ManCity
+    418,  # Real Madrid
+    27,  # Bayern
+    31,  # Liverpool
+    583,  # Paris SG
+    12,  # Roma
+    16,  # Dortmund
+    46,  # Inter
+    15,  # Leverkusen
+    1050,  # Villarreal
+    631,  # Chelsea
+    294,  # Benfica
+    23826,  # Leipzig
+    13,  # Atl. Madrid
+    131,  # Barcelona
+    720,  # FC Porto
+    985,  # ManUnited
+    11,  # Arsenal
+    800,  # Bergamo
+    379,  # West Ham
+    506,  # Juventus
+    5,  # Milan
+    2282,  # FC Brügge
+    234,  # Feyenoord
+    6195,  # Neapel
+    368,  # Sevilla
+    610,  # Ajax
+    383,  # Eindhoven
+    124,  # Rangers
+    398,  # Lazio
+]
